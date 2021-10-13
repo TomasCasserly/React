7a73df391bce09ca736722b91ff6ab36ad551f46
Tomas <tomascasserly@gmail.com>
2021-10-12 22:25:57 -0300

    Clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Lotus Argentina</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#historia">Historia</Nav.Link>
+                        <NavDropdown title="Modelos" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Elise</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Exige</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Emira</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Evija - El Futuro</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+
+    )
+}
+
+export default NavBar;
